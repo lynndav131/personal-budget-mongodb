bd2089ff9ed8e93da5ab0a98b67cdbe96a3d01fd
Lynnox Davis <ldavi131@charlotte.edu>
2025-09-13 14:48:20 -0400

    Assignment- 04 | Created dynamic doughnut chart

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 // Budget API
-
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/', express.static('public'));
 
 const budget = {
     myBudget: [
         {
-            title: 'Eat out',
-            budget: 25
+            title: 'Car Note',
+            budget: 320
         },
         {
             title: 'Rent',
-            budget: 275
+            budget: 1750
         },
         {
-            title: 'Grocery',
-            budget: 110
+            title: 'Phone',
+            budget: 150
         },
+        {
+            title: 'Doggy Daycare',
+            budget: 450
+        }
     ]
 };
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log('Example app listening at http://localhost:${port}');
 });
